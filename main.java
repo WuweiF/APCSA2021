76fbc6f87e23174b6eef4f45271b1bfcd23ca418
WuweiF <90229747+WuweiF@users.noreply.github.com>
2021-09-07 10:28:49 +0800

    add main.java of hello world
    your first program

---
+++ main.java
@@ -0,0 +1,12 @@
+package com.apcsacourse;
+
+public class main {
+
+  public static void main(String[] args) {
+   System.out.println("hello world");
+   system.out.println("Java is a cap sensitive language.");
+   //A good IDE will be helpful.
+   int a=233;
+   System.out.println(a);
+  }
+}
